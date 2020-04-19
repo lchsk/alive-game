e16ba4b85e65574df136ff583cc4e735fa718430
lchsk <mjlechowski@gmail.com>
2020-04-20 00:19:44 +0100

    Add math code

--- src/math.c
+++ src/math.c
@@ -1,5 +1,6 @@
 #include <engine212/engine.h>
 
+#include "config.h"
 #include "math.h"
 
 #define M_PI 3.14159265358979323846
@@ -8,6 +9,21 @@
 Pointd get_pos(Input *input, Entity *entity) {
             double x_diff = (input->mouse_x - entity->position.x);
             double y_diff = (input->mouse_y - entity->position.y);
+
+                  double m = fmax(fabs(x_diff), fabs(y_diff));
+
+                  double xd = x_diff / m;
+                  double yd = y_diff / m;
+
+                  double change_x = xd * 1;
+                  double change_y = yd * 1;
+
+                  return (Pointd) {change_x, change_y};
+}
+
+Pointd get_pos_entities(Pointd target, Entity *source) {
+            double x_diff = (target.x - source->position.x);
+            double y_diff = (target.y - source->position.y);
 
                   double m = fmax(fabs(x_diff), fabs(y_diff));
 
@@ -33,21 +49,35 @@ Pointd get_new_position(Input *input, Entity *entity, double delta,
                   double player_x = entity->position.x;
                   double player_y = entity->position.y;
 
-                  double change_x = xd * 1 * delta;
-                  double change_y = yd * 1 * delta;
+                  double change_x = xd * delta;
+                  double change_y = yd * delta;
 
-                  int dir_x, dir_y;
+                  double speed = 1;
+
+                  int dir_x = 1, dir_y = 1;
 
                   if (direction == 0) {
                       dir_x = dir_y = 1;
+                      speed = PLAYER_FORWARD_SPEED;
                   } else if (direction == 1) {
+                      speed = PLAYER_SIDE_SPEED;
+                      Pointd d = rotate((Pointd) {change_x, change_y}, -90);
+
+                      change_x = d.x;
+                      change_y = d.y;
                   } else if (direction == 2) {
                       dir_x = dir_y = -1;
+                      speed = PLAYER_BACKWARD_SPEED;
                   } else if (direction == 3) {
+                      speed = PLAYER_SIDE_SPEED;
+                      Pointd d = rotate((Pointd) {change_x, change_y}, 90);
+
+                      change_x = d.x;
+                      change_y = d.y;
                   }
 
-                  player_x += change_x * dir_x;
-                  player_y += change_y * dir_y;
+                  player_x += change_x * dir_x * speed;
+                  player_y += change_y * dir_y * speed;
 
                   return (Pointd) {player_x, player_y};
 
@@ -55,4 +85,18 @@ Pointd get_new_position(Input *input, Entity *entity, double delta,
 
 double rad_to_deg(double rad) {
     return rad * 180 / M_PI;
+}
+
+double get_distance(Pointd p1, Pointd p2) {
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+
+Pointd rotate(Pointd v, double angle) {
+    double x = v.x;
+    double y = v.y;
+
+    return (Pointd) {
+        cos(angle) * x - sin(angle) * y,
+            sin(angle) * x + cos(angle) * y
+            };
 }
--- src/math.h
+++ src/math.h
@@ -2,12 +2,14 @@
     #define MATH_H
 
 Pointd get_pos(Input *input, Entity *entity);
+Pointd get_pos_entities(Pointd target, Entity *source);
 Pointd get_new_position(Input *input, Entity *entity, double delta,
                         int direction);
 
+double get_distance(Pointd p1, Pointd p2);
+
 double rad_to_deg(double rad);
 
-
-
+Pointd rotate(Pointd v, double angle);
 
     #endif /* MATH_H */
