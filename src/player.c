8428bbd49db5b926b145a48f9bd6dc3426d3b422
lchsk <mjlechowski@gmail.com>
2020-04-20 00:18:47 +0100

    Add current player/game code

--- src/player.c
+++ src/player.c
@@ -4,8 +4,9 @@
 #include "math.h"
 
 void rotate_player(Entity* player, Input *input) {
-    double a = rad_to_deg(atan2(input->mouse_y - player->position.y, input->mouse_x - player->position.x));
+    double y_diff = input->mouse_y - player->position.y;
+    double x_diff = input->mouse_x - player->position.x;
 
+    double a = rad_to_deg(atan2(y_diff, x_diff));
     player->sprite->rotation = a;
-
 }
--- src/player.h
+++ src/player.h
@@ -1,6 +1,28 @@
 #ifndef PLAYER_H
     #define PLAYER_H
 
+
+typedef enum {
+    GAME_STATE_HELP,
+    GAME_STATE_PLAY,
+    GAME_STATE_END
+} GameState;
+
+typedef enum {
+    WEAPON_PISTOL,
+    WEAPON_RIFLE,
+    WEAPON_MACHINE,
+} Weapon;
+
+typedef struct PlayerState {
+    double health;
+    GameState game_state;
+    double survival_time;
+    int kills;
+    Weapon weapon;
+    double since_last_shot;
+} PlayerState;
+
 void rotate_player(Entity* player, Input *input);
 
     #endif /* PLAYER_H */
